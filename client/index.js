1e9f68f6f467c6dafc2f1d72f2bb00b0d1480401
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 18:14:40 +1200

    imported thunk in client/index.js

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import thunk from 'redux-thunk'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import reducers from './reducers'
 import App from './components/App'
 
 const store = createStore(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 )
 
 document.addEventListener('DOMContentLoaded', () => {
