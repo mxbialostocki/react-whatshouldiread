62db68095ff2637bb67d92b50243afb1642497a4
mxbialostocki <matt.bialostocki@gmail.com>
2019-08-05 13:11:42 +1200

    remove dead code in reducer

--- client/reducers/activePage.js
+++ client/reducers/activePage.js
@@ -5,18 +5,6 @@ function activePage (state = 'all', action) {
     case UPDATE_ACTIVE_PAGE:
       return action.activePage
 
-      // case ACTIVE_PAGE_SHORT_STORIES:
-      //   return activePage.shortStories
-
-      // case ACTIVE_PAGE_POETRY:
-      //   return activePage.poetry
-
-      // case ACTIVE_PAGE_AOTEAROA:
-      //   return activePage.aotearoa
-
-      // case ACTIVE_PAGE_MEMOIR:
-      //   return activePage.memoir
-
     default:
       return state
   }
