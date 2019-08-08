637b7373da5e2d22bae88cab64c15d6e66091669
mxbialostocki <matt.bialostocki@gmail.com>
2019-08-08 13:42:55 +1200

    add  activePage reset to header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <div>
-        <ul id="nav-main">
-          <li className="nav-child"><Link to="/books/review">just one book</Link></li>
-          <li className="nav-child"><Link to="/books/allcovers">all / covers</Link></li>
-          <li className="nav-child"><Link to="/books/alllist"> all / list</Link></li>
-        </ul>
-      </div>
-    )
+import { activePageAll } from '../actions/index'
+
+function Header (props) {
+  const { dispatch, activePage } = props
+
+  return (
+    <div>
+      <ul id="nav-main">
+        <li><button className="nav-child" onClick={() => dispatch(activePageAll())}><Link to="/books/review">just one book</Link></button></li>
+        <li className="nav-child"><Link to="/books/allcovers">all / covers</Link></li>
+        <li className="nav-child"><Link to="/books/alllist"> all / list</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+function mapStateToProps (state) {
+  return {
+    activePage: state.activePage
   }
 }
 
-export default Header
+export default connect(mapStateToProps)(Header)
